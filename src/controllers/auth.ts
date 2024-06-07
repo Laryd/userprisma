b7bd90cf8b17fdebab0b1c4cf62c58d19de0719e
Laryd <hillarymuboka@gmail.com>
2024-06-07 10:26:45 -0700

    feat: generic error handler

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -6,48 +6,42 @@ import { JWT_SECRET } from "../secrets";
 import { BadRequestsException } from "../exceptions/bad-requests";
 import { ErrorCode } from "../exceptions/root";
 import { UnprocessableEntity } from "../exceptions/validation";
+import { RegisterSchema } from "../schema/users";
 
 export const login = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  try {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    let user = await prismaClient.user.findFirst({
-      where: { email },
-    });
+  let user = await prismaClient.user.findFirst({
+    where: { email },
+  });
 
-    if (!user) {
-      return next(
-        new BadRequestsException(
-          "User does not exist",
-          ErrorCode.USER_NOT_FOUND
-        )
-      );
-    }
-
-    if (!compareSync(password, user.password)) {
-      return next(
-        new BadRequestsException(
-          "Incorrect password",
-          ErrorCode.INCORRECT_PASSWORD
-        )
-      );
-    }
-
-    const token = jwt.sign(
-      {
-        userId: user.id,
-      },
-      JWT_SECRET!
+  if (!user) {
+    return next(
+      new BadRequestsException("User does not exist", ErrorCode.USER_NOT_FOUND)
     );
-
-    res.json({ user, token });
-  } catch (err:any) {
-    next(new UnprocessableEntity(err?.cause?.issues, "Unprocessable entity", ErrorCode.UNPROCESSABLE_ENTITY));
   }
+
+  if (!compareSync(password, user.password)) {
+    return next(
+      new BadRequestsException(
+        "Incorrect password",
+        ErrorCode.INCORRECT_PASSWORD
+      )
+    );
+  }
+
+  const token = jwt.sign(
+    {
+      userId: user.id,
+    },
+    JWT_SECRET!
+  );
+
+  res.json({ user, token });
 };
 
 export const register = async (
@@ -55,34 +49,31 @@ export const register = async (
   res: Response,
   next: NextFunction
 ) => {
-  try {
-    const { email, password, name, phone } = req.body;
+  RegisterSchema.parse(req.body);
+  const { email, password, name, phone } = req.body;
 
-    let user = await prismaClient.user.findFirst({
-      where: { email },
-    });
+  let user = await prismaClient.user.findFirst({
+    where: { email },
+  });
 
-    if (user) {
-      return next(
-        new BadRequestsException(
-          "User already exists",
-          ErrorCode.USER_ALREADY_EXISTS
-        )
-      );
-    }
-
-    user = await prismaClient.user.create({
-      data: {
-        name,
-        email,
-        password: hashSync(password, 10),
-        phone,
-        isAdmin: false,
-      },
-    });
-
-    res.json(user);
-  } catch (error) {
-    next(error);
+  if (user) {
+    return next(
+      new BadRequestsException(
+        "User already exists",
+        ErrorCode.USER_ALREADY_EXISTS
+      )
+    );
   }
+
+  user = await prismaClient.user.create({
+    data: {
+      name,
+      email,
+      password: hashSync(password, 10),
+      phone,
+      isAdmin: false,
+    },
+  });
+
+  res.json(user);
 };
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { login, register } from "../controllers/auth";
+import { errorHandler } from "../error-handler";
 
 
 const authRoutes:Router = Router()
 
-authRoutes.post('/login', login)
+authRoutes.post('/login', errorHandler(login))
 
-authRoutes.post("/register", register)
+authRoutes.post("/register", errorHandler(register))
 export default authRoutes
