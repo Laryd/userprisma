0bdac43a0952e8cb709238b56d558b395c5c3cc3
Laryd <hillarymuboka@gmail.com>
2024-06-08 00:19:14 -0700

    feat:controllers for auth route

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -6,48 +6,39 @@ import { JWT_SECRET } from "../secrets";
 import { BadRequestsException } from "../exceptions/bad-requests";
 import { ErrorCode } from "../exceptions/root";
 import { UnprocessableEntity } from "../exceptions/validation";
+import { RegisterSchema } from "../schema/users";
+import { NotFoundException } from "../exceptions/not-found";
 
 export const login = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  try {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    let user = await prismaClient.user.findFirst({
-      where: { email },
-    });
+  let user = await prismaClient.user.findFirst({
+    where: { email },
+  });
 
-    if (!user) {
-      return next(
-        new BadRequestsException(
-          "User does not exist",
-          ErrorCode.USER_NOT_FOUND
-        )
-      );
-    }
-
-    if (!compareSync(password, user.password)) {
-      return next(
-        new BadRequestsException(
-          "Incorrect password",
-          ErrorCode.INCORRECT_PASSWORD
-        )
-      );
-    }
-
-    const token = jwt.sign(
-      {
-        userId: user.id,
-      },
-      JWT_SECRET!
-    );
-
-    res.json({ user, token });
-  } catch (err:any) {
-    next(new UnprocessableEntity(err?.cause?.issues, "Unprocessable entity", ErrorCode.UNPROCESSABLE_ENTITY));
+  if (!user) {
+    throw new NotFoundException("User not found", ErrorCode.USER_NOT_FOUND);
   }
+
+  if (!compareSync(password, user.password)) {
+    throw new BadRequestsException(
+      "Incorrect password",
+      ErrorCode.INCORRECT_PASSWORD
+    );
+  }
+
+  const token = jwt.sign(
+    {
+      userId: user.id,
+    },
+    JWT_SECRET!
+  );
+
+  res.json({ user, token });
 };
 
 export const register = async (
@@ -55,34 +46,134 @@ export const register = async (
   res: Response,
   next: NextFunction
 ) => {
-  try {
-    const { email, password, name, phone } = req.body;
+  RegisterSchema.parse(req.body);
+  const { email, password, name, phone } = req.body;
 
-    let user = await prismaClient.user.findFirst({
-      where: { email },
-    });
+  let user = await prismaClient.user.findFirst({
+    where: { email },
+  });
 
-    if (user) {
-      return next(
-        new BadRequestsException(
-          "User already exists",
-          ErrorCode.USER_ALREADY_EXISTS
-        )
-      );
-    }
-
-    user = await prismaClient.user.create({
-      data: {
-        name,
-        email,
-        password: hashSync(password, 10),
-        phone,
-        isAdmin: false,
-      },
-    });
-
-    res.json(user);
-  } catch (error) {
-    next(error);
+  if (user) {
+    throw new BadRequestsException(
+      "User already exists",
+      ErrorCode.USER_ALREADY_EXISTS
+    );
   }
+
+  user = await prismaClient.user.create({
+    data: {
+      name,
+      email,
+      password: hashSync(password, 10),
+      phone,
+      isAdmin: false,
+    },
+  });
+
+  res.json(user);
+};
+
+// /me => return logged in user
+export const me = async (
+  req: Request,
+  res: Response,
+) => {
+  console.log(req.user);
+  res.json(req.user);
+  
+};
+
+// Retrieve all users
+export const getUsers = async (req: Request, res: Response, next: NextFunction) => {
+  const users = await prismaClient.user.findMany();
+  res.json(users);
+};
+
+// Create a new user
+export const createUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { email, password, name, phone, isAdmin } = req.body;
+
+  let user = await prismaClient.user.findFirst({
+    where: { email },
+  });
+
+  if (user) {
+    throw new BadRequestsException(
+      "User already exists",
+      ErrorCode.USER_ALREADY_EXISTS
+    );
+  }
+
+  user = await prismaClient.user.create({
+    data: {
+      name,
+      email,
+      password: hashSync(password, 10),
+      phone,
+      isAdmin: !!isAdmin,
+    },
+  });
+
+  res.json(user);
+}
+
+// Retrieve a specific user by ID
+export const getUserById = async (req: Request, res: Response, next: NextFunction) => {
+  const { id } = req.params;
+
+  const user = await prismaClient.user.findUnique({
+    where: { id: Number(id) },
+  });
+
+  if (!user) {
+    throw new NotFoundException("User not found", ErrorCode.USER_NOT_FOUND);
+  }
+
+  res.json(user);
+};
+
+// Update an existing user
+export const updateUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { id } = req.params;
+  const { email, password, name, phone, isAdmin } = req.body;
+
+  let user = await prismaClient.user.findUnique({
+    where: { id: Number(id) },
+  });
+
+  if (!user) {
+    throw new NotFoundException("User not found", ErrorCode.USER_NOT_FOUND);
+  }
+
+  user = await prismaClient.user.update({
+    where: { id: Number(id) },
+    data: {
+      name,
+      email,
+      password: password ? hashSync(password, 10) : user.password,
+      phone,
+      isAdmin: isAdmin !== undefined ? !!isAdmin : user.isAdmin,
+    },
+  });
+
+  res.json(user);
+};
+
+// Delete a user from the database
+export const deleteUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { id } = req.params;
+
+  const user = await prismaClient.user.findUnique({
+    where: { id: Number(id) },
+  });
+
+  if (!user) {
+    throw new NotFoundException("User not found", ErrorCode.USER_NOT_FOUND);
+  }
+
+  await prismaClient.user.delete({
+    where: { id: Number(id) },
+  });
+
+  res.json({ message: "User deleted successfully" });
 };
