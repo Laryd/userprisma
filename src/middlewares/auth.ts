847fb12824830f0f53e392ebccaafcc34c0ddd43
Laryd <hillarymuboka@gmail.com>
2024-06-08 00:21:32 -0700

    feat: add authentication middleware

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,52 @@
+import { NextFunction, Request, Response } from "express";
+import { UnauthorizedException } from "../exceptions/unauthorized";
+import { ErrorCode } from "../exceptions/root";
+import { JWT_SECRET } from "../secrets";
+import { prismaClient } from "..";
+import * as jwt from "jsonwebtoken";
+
+const authMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  // 1. extract the token from header
+  const token = req.headers.authorization;
+  // 2. if token is not present, throw an error of unauthorized
+  if (!token) {
+    next(new UnauthorizedException("Unauthorized", ErrorCode.UNAUTHORIZED));
+  }
+  try {
+    // if token is present, verify that token and etract the payload
+    const payload = jwt.verify(token!, JWT_SECRET) as any;
+    // get the user for the payload
+    const user = await prismaClient.user.findFirst({
+      where: {
+        id: payload.userId,
+      },
+    });
+    if (!user) {
+      next(new UnauthorizedException("Unauthorized", ErrorCode.UNAUTHORIZED));
+    }
+    // attach user to the current request object
+    console.log(user)
+    req.user = user;
+    console.log(req.user)
+    next();
+  } catch (error) {
+    next(new UnauthorizedException("Unauthorized", ErrorCode.UNAUTHORIZED));
+  }
+};
+
+// Middleware to check if user is admin
+export const isAdmin = (req: Request, res: Response, next: NextFunction) => {
+  if (!req.user?.isAdmin) {
+    throw new UnauthorizedException(
+      "Admin privileges required",
+      ErrorCode.UNAUTHORIZED
+    );
+  }
+  next();
+};
+
+export default authMiddleware;
