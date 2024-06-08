2fedca386dceec982e95e68e6f5b8774f19a7276
Laryd <hillarymuboka@gmail.com>
2024-06-08 00:27:09 -0700

    feat: add user routes

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,10 +1,22 @@
 import { Router } from "express";
-import { login, register } from "../controllers/auth";
+import { createUser, deleteUser, getUserById, getUsers, login, me, register, updateUser } from "../controllers/auth";
+import { errorHandler } from "../error-handler";
+import authMiddleware, { isAdmin } from "../middlewares/auth";
 
 
 const authRoutes:Router = Router()
 
-authRoutes.post('/login', login)
+authRoutes.post('/login', errorHandler(login))
 
-authRoutes.post("/register", register)
+authRoutes.post("/register", errorHandler(register))
+
+authRoutes.get("/me", [authMiddleware], errorHandler(me));
+
+// User routes
+authRoutes.get("/users", [authMiddleware], errorHandler(getUsers));
+authRoutes.post("/users", [authMiddleware, isAdmin], errorHandler(createUser));
+authRoutes.get("/users/:id", [authMiddleware], errorHandler(getUserById));
+authRoutes.put("/users/:id", [authMiddleware], errorHandler(updateUser));
+authRoutes.delete("/users/:id", [authMiddleware], errorHandler(deleteUser));
+
 export default authRoutes
